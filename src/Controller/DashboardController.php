78810667761ecb6137303df67fd4d3a2b15cc8ed
yaliilyaya <yaliilyaya@gmai.ru>
2022-05-30 00:22:38 +0300

    DashboardController

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -1,10 +1,6 @@
 <?php
 namespace App\Controller;
 
-use App\Entity\Item;
-use App\Repository\ItemRepository;
-use App\Repository\RecipeRepository;
-use App\Srevice\BuilderAtlasImagesService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,7 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
  * @package App\Controller
  * @see Route
  */
-class IndexController extends AbstractController
+class DashboardController extends AbstractController
 {
     /**
      * @Route("/", name="index")
@@ -24,7 +20,7 @@ class IndexController extends AbstractController
     {
         $items = [];
 
-        return $this->render('index/list.html.twig', [
+        return $this->render('index/dashboard.html.twig', [
             'items' => $items
         ]);
     }
