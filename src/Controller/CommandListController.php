8bbf0b6d4258724f6ab56ff5621f45c6e8b7a3ad
yaliilyaya <yaliilyaya@gmai.ru>
2022-06-01 22:58:32 +0300

    CommandListController.php

---
+++ src/Controller/CommandListController.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Controller;
+
+use App\Entity\ServiceCommand;
+use App\Repository\ServiceConnectionRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class IndexController
+ * @package App\Controller
+ * @see Route
+ */
+class CommandListController extends AbstractController
+{
+    /**
+     * @Route("/commands/{serviceId}", name="commands")
+     * @return Response
+     */
+    public function commands(ServiceConnectionRepository $serviceConnectionRepository): Response
+    {
+        $commands = [
+            new ServiceCommand()
+        ];
+
+        return $this->render('index/commands.html.twig', [
+            'commands' => $commands
+        ]);
+    }
+}
---
+++ src/Entity/ServiceCommand.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\Index;
+
+/**
+ * @ORM\Entity(repositoryClass=ServerOptionRepository::class)
+ */
+class ServiceCommand
+{
+    const STATUS_INFO_TYPE = 'info';
+    const STATUS_SUCCESS_TYPE = 'success';
+    const STATUS_WARNING_TYPE = 'warning';
+    const STATUS_DANGER_TYPE = 'danger';
+
+    /**
+     * @var int
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+    /**
+     * @var string
+     * @ORM\Column(type="string")
+     */
+    private $type;
+    /**
+     * @var string
+     * @ORM\Column(type="string")
+     */
+    private $status;
+    /**
+     * @var array
+     * @ORM\Column(type="json")
+     */
+    private $data = [];
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id ?? 0;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType(): string
+    {
+        return $this->type ?? '';
+    }
+
+    /**
+     * @param string $type
+     */
+    public function setType(string $type): void
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStatus(): string
+    {
+        return $this->status ?? '';
+    }
+
+    /**
+     * @param string $status
+     */
+    public function setStatus(string $status): void
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * @return array|null
+     */
+    public function getData(): ?array
+    {
+        return $this->data ?? [];
+    }
+
+    /**
+     * @param array $data
+     * @return $this
+     */
+    public function setData(array $data): self
+    {
+        $this->data = $data;
+
+        return $this;
+    }
+
+    /**
+     * @param $field
+     * @return mixed|null
+     */
+    public function __get($field)
+    {
+        return $this->data[$field] ?? null;
+    }
+
+    /**
+     * @param $field
+     * @param $value
+     * @return void
+     */
+    public function __set($field, $value)
+    {
+        $this->data[$field] = $value;
+    }
+
+    /**
+     * @param $field
+     * @return bool
+     */
+    public function __isset($field)
+    {
+        return isset($this->data[$field]);
+    }
+
+    /**
+     * @return array
+     */
+    public function toArray(): array
+    {
+        return [
+            'id' => $this->getId(),
+            'name' => $this->getName(),
+            'ip' => $this->getIp(),
+            'data' => $this->getData(),
+        ];
+    }
+}
