6d9639ba48e3536c9f21e78a4685d2a185e95bd0
yaliilyaya <yaliilyaya@gmai.ru>
2022-06-05 23:17:13 +0300

    ExtractorConfigServer

--- src/Srevice/ExtractorConfigServer.php
+++ src/Srevice/ExtractorConfigServer.php
@@ -2,11 +2,26 @@
 
 namespace App\Srevice;
 
+use App\Entity\ServiceCommand;
 use App\Entity\ServiceConnection;
 use App\Entity\ServiceJob;
+use App\Repository\ServiceCommandRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 
 class ExtractorConfigServer
 {
+    /**
+     * @var ServiceCommandRepository
+     */
+    private $serviceCommandRepository;
+
+    public function __construct(ServiceCommandRepository $serviceCommandRepository)
+    {
+
+        $this->serviceCommandRepository = $serviceCommandRepository;
+    }
+
     /**
      * @param ServiceConnection $service
      * @return array
@@ -15,13 +30,42 @@ class ExtractorConfigServer
     {
         $serviceConfig = $service->toArray();
 
-        $jobConfigs = $service->getJobs()->map(function (ServiceJob $job) {
-            return [
-                $job->getType() => $job->toArray()
-            ];
-        })->toArray();
+        $commandConfigs = $this->extractCommandConfigs($this->serviceCommandRepository->findAll());
+        $jobConfigs = $this->extractJobConfigs($service->getJobs());
 
         $serviceConfig['jobs'] = $jobConfigs;
         return $serviceConfig;
+    }
+
+    /**
+     * @param Collection $jobs
+     * @return array
+     */
+    private function extractJobConfigs(Collection $jobs): array
+    {
+        $jobConfigs = $jobs->map(function (ServiceJob $job) {
+            return [
+                $job->getCommand()->getType() => $job->toArray()
+            ];
+        })->toArray();
+
+        return $jobConfigs ? array_merge(...$jobConfigs) : [];
+    }
+
+    /**
+     * @param array $commands
+     * @return array
+     */
+    private function extractCommandConfigs(array $commands): array
+    {
+        $commandCollection = new ArrayCollection($commands);
+
+        $commandConfigs = $commandCollection->map(function (ServiceCommand $command) {
+            return [
+                $command->getType() => $command->toArray()
+            ];
+        })->toArray();
+
+        return $commandConfigs ? array_merge(...$commandConfigs) : [];
     }
 }
