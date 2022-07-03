996da2d8eea3abccc09cddcb3309de902bdda1e5
i.didenko <didenko.i.a@effective-group.ru>
2022-07-03 19:23:47 +0300

    DfCommand

---
+++ src/Command/DfCommand.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\ServiceJob;
+use App\Entity\ServiceJobReport;
+use App\Enum\StatusEnum;
+use App\Model\RemoteCommand;
+use App\Model\RemoteFileCommand;
+use App\Repository\ServiceJobReportRepository;
+use App\Srevice\CommandRunnerService;
+
+class DfCommand implements CommandInterface
+{
+
+    /**
+     * @var CommandRunnerService
+     */
+    private $commandRunnerService;
+
+    /**
+     * @param CommandRunnerService $commandRunnerService
+     */
+    public function __construct(
+        CommandRunnerService $commandRunnerService
+    ) {
+        $this->commandRunnerService = $commandRunnerService;
+    }
+
+    /**
+     * @param ServiceJob $serviceJob
+     * @return ServiceJobReport
+     */
+    public function run(ServiceJob $serviceJob): ServiceJobReport
+    {
+        $connection = $serviceJob->getConnection();
+
+        $remoteFileCommand = new RemoteCommand();
+
+        $remoteFileCommand->setCommandAttribute(['df', '-h']);
+        $remoteFileCommand->setConnectParam([
+            $connection->getUser(),
+            $connection->getIp(),
+            $connection->getPassword()
+        ]);
+
+        return $this->commandRunnerService->run($remoteFileCommand);
+    }
+}
