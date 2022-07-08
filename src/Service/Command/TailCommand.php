b78a5c8c2d51c46b4405d8b774046c7d7821f633
i.didenko <didenko.i.a@effective-group.ru>
2022-07-08 10:41:50 +0300

    TailCommand

--- src/Service/Command/TailCommand.php
+++ src/Service/Command/TailCommand.php
@@ -5,7 +5,9 @@ namespace App\Service\Command;
 use App\Collection\ServiceJobReportCollection;
 use App\Entity\ServiceJob;
 use App\Entity\ServiceJobReport;
+use App\Enum\StatusEnum;
 use App\Model\RemoteCommand;
+use App\Repository\ServiceJobReportRepository;
 use App\Service\CommandRunnerService;
 use Doctrine\Common\Collections\ArrayCollection;
 
@@ -16,14 +18,20 @@ class TailCommand implements CommandInterface
      * @var CommandRunnerService
      */
     private $commandRunnerService;
+    /**
+     * @var ServiceJobReportRepository
+     */
+    private $serviceJobReportRepository;
 
     /**
      * @param CommandRunnerService $commandRunnerService
      */
     public function __construct(
-        CommandRunnerService $commandRunnerService
+        CommandRunnerService $commandRunnerService,
+        ServiceJobReportRepository $serviceJobReportRepository
     ) {
         $this->commandRunnerService = $commandRunnerService;
+        $this->serviceJobReportRepository = $serviceJobReportRepository;
     }
 
     /**
@@ -43,8 +51,14 @@ class TailCommand implements CommandInterface
             $connection->getPassword()
         ];
 
-        $reports = new ServiceJobReportCollection();
+        if (!$files) {
+            $report = $this->serviceJobReportRepository->create();
+            $report->setStatus(StatusEnum::ERROR_TYPE);
+            $report->setResult('Empty $files');
+            return  new ServiceJobReportCollection([$report]);
+        }
 
+        $reports = new ServiceJobReportCollection();
         foreach ($files as $file) {
             $report = $this->execRemoteFileContent($file, $params);
             $reports->add($report);
